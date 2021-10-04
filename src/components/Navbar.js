ac3ef3a4311c7a03fa34eef9133fd06915702ca7
Gabriel Bordenabe <86176041+gbordenabe@users.noreply.github.com>
2021-10-04 16:46:56 -0300

    second commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,20 @@
-import React, {useState} from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import logo from '../assets/HollowKnight_logo.webp';
-import { ShoppingCart } from '@material-ui/icons';
-import { Badge } from '@material-ui/core';
-import {Link} from "react-router-dom";
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import logo from "../assets/HollowKnight_logo.webp";
+import { ShoppingCart } from "@material-ui/icons";
+import { Badge } from "@material-ui/core";
+import { Link } from "react-router-dom";
 import { useStateValue } from "../StateProvider";
-import { auth } from '../firebase';
-import { actionTypes } from '../reducer';
-import { useHistory } from 'react-router';
-import DrawerCart from '../components/DrawerCart'
+import { auth } from "../firebase";
+import { actionTypes } from "../reducer";
+import { useHistory } from "react-router";
+import DrawerCart from "../components/DrawerCart";
+import { logRoles } from "@testing-library/dom";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -27,79 +28,88 @@ const useStyles = makeStyles((theme) => ({
   grow: {
     flexGrow: 1,
   },
-  button:{
-      marginLeft: theme.spacing(2),
+  button: {
+    marginLeft: theme.spacing(2),
   },
-  image:{
-    marginRight:"10px",
-    height:"2rem",
+  image: {
+    marginRight: "10px",
+    height: "2rem",
   },
 }));
 
 export default function Navbar() {
   const classes = useStyles();
-  const [{basket, user}, dispatch] = useStateValue();
+  const [{ basket, user }, dispatch] = useStateValue();
   const [openDrawerCart, setDrawerCart] = useState(false);
   const history = useHistory();
 
-  const hanbleOpenDrowerCart = () =>{
+  const hanbleOpenDrowerCart = () => {
     setDrawerCart(true);
-  }
+  };
 
-  const hanbleCloseDrowerCart = () =>{
+  const hanbleCloseDrowerCart = () => {
     setDrawerCart(false);
-  }
+  };
 
-  const handleAuth = () =>{
-    if(user){
+  const handleAuth = () => {
+    if (user) {
       auth.signOut();
       dispatch({
         type: actionTypes.EMPTY_BASKET,
-        basket:[],
-      })
+        basket: [],
+      });
       dispatch({
         type: actionTypes.SET_USER,
         user: null,
-      })
+      });
       history.push("/");
     }
-  }
+  };
 
   return (
     <div className={classes.root}>
       <AppBar position="fixed" className={classes.appBar}>
         <Toolbar>
-            <Link to="/">
-                <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                    <img className={classes.image} src={logo}/>
-                </IconButton>
-            </Link>
+          <Link to="/">
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="menu"
+            >
+              <img className={classes.image} src={`./${logo}`} />
+            </IconButton>
+          </Link>
           <div className={classes.grow} />
           <Typography variant="h6" color="textPrimary" component="p">
             Hello {user ? user.email : "Guest"}
           </Typography>
           <div className={classes.button}>
-            <Link to="/singin" style={{ textDecoration: 'none' }}>
-            <Button variant="outlined" onClick={handleAuth}>
-              <strong>{user ? "Sing Out" : "Sing In"}</strong>
+            <Link to="/singin" style={{ textDecoration: "none" }}>
+              <Button variant="outlined" onClick={handleAuth}>
+                <strong>{user ? "Sing Out" : "Sing In"}</strong>
               </Button>
             </Link>
-              {/* <Link to="/checkout-page">
+            {/* <Link to="/checkout-page">
                 <IconButton aria-label="show cart items" color="inherit">
                     <Badge badgeContent={basket?.length} color="secondary">
                         <ShoppingCart fontSize="large" color="primary"/>
                     </Badge>
                 </IconButton>
               </Link> */}
-                <IconButton onClick={hanbleOpenDrowerCart} aria-label="show cart items" color="inherit">
-                    <Badge badgeContent={basket?.length} color="secondary">
-                        <ShoppingCart fontSize="large" color="primary"/>
-                    </Badge>
-                </IconButton>
-            </div>
+            <IconButton
+              onClick={hanbleOpenDrowerCart}
+              aria-label="show cart items"
+              color="inherit"
+            >
+              <Badge badgeContent={basket?.length} color="secondary">
+                <ShoppingCart fontSize="large" color="primary" />
+              </Badge>
+            </IconButton>
+          </div>
         </Toolbar>
       </AppBar>
-      <DrawerCart open={openDrawerCart} close={hanbleCloseDrowerCart}/>
+      <DrawerCart open={openDrawerCart} close={hanbleCloseDrowerCart} />
     </div>
   );
 }
